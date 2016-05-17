cf3a8308a4da880bf00cc51ea60cb5cbd7188202
pktzj117 <pktzj117@gmail.com>
2016-05-17 17:43:43 +0800

    1.0bate

---
+++ app/src/main/java/org/pktzj/mobilesafe/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package org.pktzj.mobilesafe.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.pktzj.mobilesafe.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/org/pktzj/mobilesafe/activity/SplashActivity.java
+++ app/src/main/java/org/pktzj/mobilesafe/activity/SplashActivity.java
@@ -1,10 +1,17 @@
 package org.pktzj.mobilesafe.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.animation.AlphaAnimation;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,14 +28,70 @@ import java.net.URL;
 public class SplashActivity extends Activity {
 
     private static final String TAG = "SplashActivity";
+    private static final int NOERROR = -1;
+    private static final int LOADMAIN = 1;//进入主界面
+    private static final int SHOWUPDATEDIALOG = 2;//弹出更新对话框
+    private static final int ERROR = 3;//错误统一代号
     private RelativeLayout rl_root;
+    private TextView tv_versionName;
+    private int versionCode;
+    private String versionName;
+    private long startTimeMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //初始化界面
         initView();
+        //初始化数据
+        initData();
+        //c初始化动画
         initAnimation();
+        //检测服务器的版本
         checkVersion();
+    }
+
+    private void initData() {
+        //获取自己的版本信息
+        PackageManager pm = getPackageManager();
+
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(getPackageName(), 0);
+            //版本号
+            versionCode = packageInfo.versionCode;
+            //版本名
+            versionName = packageInfo.versionName;
+            Log.d(TAG, "版本名: " + versionName + "  版本号: " + versionCode + "");
+            tv_versionName.setText(versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            //can not reach
+        }
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            //处理消息
+            switch (msg.what) {
+                case LOADMAIN://加载主界面
+                    loadMain();
+                    break;
+                case ERROR:
+                    break;
+                case SHOWUPDATEDIALOG://显示更新版本的对话框
+                    showUpdateDialog();
+                    break;
+            }
+        }
+    };
+
+    private void loadMain() {
+        Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void showUpdateDialog() {
     }
 
     private void checkVersion() {
@@ -40,7 +103,9 @@ public class SplashActivity extends Activity {
                 BufferedReader bfr = null;
                 HttpURLConnection conn = null;
                 UrlBean parseJson = null;
+                int error = NOERROR;//正常码
                 try {
+                    startTimeMillis = System.currentTimeMillis();
                     URL url = new URL("http://192.168.1.101/safejson");
                     conn = (HttpURLConnection) url.openConnection();
                     conn.setReadTimeout(5 * 1000);
@@ -61,14 +126,51 @@ public class SplashActivity extends Activity {
                         //返回数据封装信息
                         parseJson = parseJson(json);
                         Log.d(TAG, parseJson.toString());
+                    } else {
+                        error = 404;
                     }
                 } catch (IOException e) {
+                    error = 4001;
                     e.printStackTrace();
                 } catch (JSONException e) {
+                    error = 4002;
                     e.printStackTrace();
+                } finally {
+                    Message msg = Message.obtain();
+                    if (error == NOERROR) {
+                        msg.what = isNewVersion(parseJson);//检测是否有新版本
+                    } else {
+                        msg.what = ERROR;
+                        msg.arg1 = error;
+                    }
+                    long endTime = System.currentTimeMillis();
+                    if (endTime - startTimeMillis < 4000) {
+                        SystemClock.sleep(3000 - (endTime - startTimeMillis));
+                        //时间不足4秒,补足4秒
+                    }
+                    handler.sendMessage(msg);//发送消息
+                    try {
+                        //关闭连接资源
+                        if (bfr == null && conn == null) {
+                            return;
+                        }
+                        bfr.close();
+                        conn.disconnect();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }.start();
+    }
+
+    private int isNewVersion(UrlBean parseJson) {
+        int serverCode = parseJson.getVersionCode();//获得服务器的版本
+        if (serverCode == versionCode) {
+            return LOADMAIN;//进入主界面
+        } else {
+            return SHOWUPDATEDIALOG;//有版本更新
+        }
     }
 
     private UrlBean parseJson(StringBuilder json) throws JSONException {
@@ -96,5 +198,6 @@ public class SplashActivity extends Activity {
     private void initView() {
         setContentView(R.layout.activity_splash);
         rl_root = (RelativeLayout) findViewById(R.id.rl_root);
+        tv_versionName = (TextView) findViewById(R.id.tv);
     }
 }
